77c126778bf61e12d3b6f1d42946b6b89edab199
lisatricy <lisatricy@gmail.com>
2025-02-04 04:02:08 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to open the link in a new tab
+function openLink() {
+  const url = "https://sites.google.com/d/1NrUBAtJ9VHoiFWlCQUXIQ3IVFJFO_bRy/p/1ak22JFMLMH0CVXzA5vD-hIRP_ZEZN-90/edit";
+  window.open(url, "_blank"); // Opens link in a new tab
+}
